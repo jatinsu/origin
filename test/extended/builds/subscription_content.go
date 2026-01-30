f8238e6493c7c2c216cc96c08ce174223af983cb
Jatin Suri <jsuri@redhat.com>
2026-01-30 16:11:13 -0500

    Prevented subscripiton tests from running on OKD clusters

--- test/extended/builds/subscription_content.go
+++ test/extended/builds/subscription_content.go
@@ -1,7 +1,9 @@
 package builds
 
 import (
+	"context"
 	"path/filepath"
+	"strings"
 
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
@@ -9,9 +11,22 @@ import (
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	admissionapi "k8s.io/pod-security-admission/api"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
 
 	exutil "github.com/openshift/origin/test/extended/util"
 )
+
+// Check for the existence of the okd-scos string in the version name to determine if it is OKD
+func isOKD(oc *exutil.CLI) (bool, error) {
+	current, err := exutil.GetCurrentVersion(context.TODO(), oc.AdminConfig())
+	if err != nil {
+		return false, err
+	}
+	if strings.Contains(current, "okd-scos") {
+		return true, nil
+	}
+	return false, nil
+}
 
 var _ = g.Describe("[sig-builds][Feature:Builds][subscription-content] builds installing subscription content", func() {
 
@@ -34,12 +49,20 @@ var _ = g.Describe("[sig-builds][Feature:Builds][subscription-content] builds in
 		})
 
 		g.JustBeforeEach(func(ctx g.SpecContext) {
+			isokd, err := isOKD(oc)
+			if err != nil {
+				e2e.Failf("Failed to get clusterversion to determine if release is OKD: %v", err)
+			}
+			if isokd {
+				g.Skip("Skipping on OKD cluster as subscription content is not available on OKD")
+			}
+
 			g.By("copying entitlement keys to namespace")
 			// The Insights Operator is responsible for retrieving the entitlement keys for the
 			// cluster and syncing them to the openshift-config-managed namespace.
 			// If this secret is not present, it means the cluster is not a Red Hat subscribed
 			// cluster and is not eligible to include entitled RHEL content in builds.
-			_, err := oc.AdminKubeClient().CoreV1().Secrets("openshift-config-managed").Get(ctx, "etc-pki-entitlement", metav1.GetOptions{})
+			_, err = oc.AdminKubeClient().CoreV1().Secrets("openshift-config-managed").Get(ctx, "etc-pki-entitlement", metav1.GetOptions{})
 			if kerrors.IsNotFound(err) {
 				g.Skip("cluster entitlements not found")
 			}
